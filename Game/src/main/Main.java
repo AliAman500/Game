55404a46668a27587f5cd66ca77f7c6d7f8963bc
Ali Aman <ali.burki1@hotmail.com>
2020-06-09 20:24:04 -0400

    Second

--- Game/src/main/Main.java
+++ Game/src/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Engine.init();
-		Window.create("Game", 900, 600, true, false, false);
+		Window.create("Game", 900, 6ssss00, true, false, false);
 		GameLoop loop = new GameLoop(60, 60);
 		loop.start();
 	}
